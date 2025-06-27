f21ed39215dca073131168bf6d8e7c7b004d80ff
ConnorDBurge <connor.d.burge@gmail.com>
2025-06-27 15:12:06 -0400

    initing project

---
+++ src/test/java/com/btamis/directoryservice/DirectoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.btamis.directoryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DirectoryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
